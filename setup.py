4763016355fd16ba2b9d3ca2a3355b24743f6c5a
Jon Bringhurst <jbringhu@linkedin.com>
2017-02-09 08:50:09 -0800

    Update versions of flake8 and nose.

--- setup.py
+++ setup.py
@@ -144,8 +144,8 @@ setup(
         'testfixtures',
     ],
     setup_requires=[
-        'nose>=1.0',
-        'flake8==2.6.2',
+        'nose>=1.3.4',
+        'flake8>=3.3.0',
     ],
     test_suite="nose.collector",
     extras_require={
