b4d894ac340b0767fc64b7e87c62d87e35c27385
Todd Palino <tpalino@linkedin.com>
2017-03-01 08:58:34 -0800

    Add JMX authentication (#57)
    * Add JMX authentication
    
    Add username/password auth support for the JMX sizer, as well as tests to support it. Also add the ability to set the truststore and password so that we can
    support self-signed SSL certificates on the JMX endpoint
    
    * Reduce code complexity in JMX sizer

--- kafka/tools/assigner/sizers/jmx.py
+++ kafka/tools/assigner/sizers/jmx.py
@@ -1,5 +1,5 @@
 from kafka.tools.assigner import log
-from kafka.tools.assigner.exceptions import UnknownBrokerException
+from kafka.tools.assigner.exceptions import UnknownBrokerException, ConfigurationException
 from kafka.tools.assigner.sizers import SizerModule
 
 import jpype
@@ -9,9 +9,15 @@ class SizerJMX(SizerModule):
     name = 'jmx'
     helpstr = 'Get partition sizes by connection to each broker via JMX'
 
-    def __init__(self, args, cluster, java_provider=None):
-        super(SizerJMX, self).__init__(args, cluster)
+    def _validate_both_properties(self, prop1, prop2):
+        if prop1 in self.properties and prop2 in self.properties:
+            return True
+        elif prop1 in self.properties or prop2 in self.properties:
+            raise ConfigurationException("JMX sizer requires both {0} and {1} properties, or neither of them".format(prop1, prop2))
+        else:
+            return False
 
+    def _set_java_provider(self, java_provider):
         if java_provider is None:
             self._java_provider = jpype
             if 'libjvm' in self.properties:
@@ -20,6 +26,21 @@ class SizerJMX(SizerModule):
                 self._java_provider.startJVM("/export/apps/jdk/JDK-1_8_0_72/jre/lib/amd64/server/libjvm.so")
         else:
             self._java_provider = java_provider
+
+    def __init__(self, args, cluster, java_provider=None):
+        super(SizerJMX, self).__init__(args, cluster)
+        self._set_java_provider(java_provider)
+
+        # If username or password is provided, you must have both
+        self._envhash = self._java_provider.java.util.HashMap()
+        if self._validate_both_properties('jmxuser', 'jmxpass'):
+            jarray = self._java_provider.JArray(self._java_provider.java.lang.String)([self.properties['jmxuser'], self.properties['jmxpass']])
+            self._envhash.put(self._java_provider.javax.management.remote.JMXConnector.CREDENTIALS, jarray)
+
+        # If truststore or truststorepass is provided, you must have both
+        if self._validate_both_properties('truststore', 'truststorepass'):
+            self._java_provider.java.lang.System.setProperty("javax.net.ssl.trustStore", self.properties['truststore'])
+            self._java_provider.java.lang.System.setProperty("javax.net.ssl.trustStorePassword", self.properties['truststorepass'])
 
     def _fetch_bean(self, connection, bean):
         topic = bean.getKeyProperty("topic")
@@ -35,7 +56,7 @@ class SizerJMX(SizerModule):
             log.info("Getting partition sizes via JMX for {0}".format(broker.hostname))
             jmxurl = self._java_provider.javax.management.remote.JMXServiceURL(
                 "service:jmx:rmi:///jndi/rmi://{0}:{1}/jmxrmi".format(broker.hostname, broker.jmx_port))
-            jmxsoc = self._java_provider.javax.management.remote.JMXConnectorFactory.connect(jmxurl)
+            jmxsoc = self._java_provider.javax.management.remote.JMXConnectorFactory.connect(jmxurl, self._envhash)
 
             connection = jmxsoc.getMBeanServerConnection()
             beans = connection.queryNames(self._java_provider.javax.management.ObjectName("kafka.log:name=Size,*"), None)
